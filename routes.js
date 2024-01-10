ad6b830bf7e60787d4fd6592459b01befab77cd3
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-01-10 16:43:30 -0300

    Adiciona cadastro (ajustar)

--- routes.js
+++ routes.js
@@ -8,16 +8,23 @@ const ControllerAmigos = require("./src/controllers/ControllerAmigos");
 const ControllerNotificacoes = require("./src/controllers/ControllerNotificacoes");
 const ControllerPerfil = require("./src/controllers/ControllerPerfil");
 const ControllerModalCadastro = require("./src/controllers/ControllerModalCadastro");
+const ControllerCadastro = require("./src/controllers/ControllerCadastro");
 
+//Routes index
 routes.get("/", ControllerIndex.paginaIndex);
 routes.get("/modal-cadastro", ControllerModalCadastro.modalCadastro);
+routes.post("/register", ControllerCadastro.register);
 
+//Routes home
 routes.get("/home", ControllerHome.paginaHome);
 
+//Routes amigos
 routes.get("/amigos", ControllerAmigos.paginaAmigos);
 
+//Routes notificações
 routes.get("/notificacoes", ControllerNotificacoes.paginaNotificacoes);
 
+//Routes perfil
 routes.get("/perfil", ControllerPerfil.paginaPerfil);
 
 module.exports = routes;
