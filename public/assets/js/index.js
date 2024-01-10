11cdd8426e983cd95ee355b2c3b27cdf0e2fb776
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-01-10 09:32:39 -0300

    Ajusta modal

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -2,24 +2,54 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnCadastro = document.getElementById("btn-cadastro");
   const content = document.getElementById("content");
   const div = document.createElement("div");
-
-  const cssTag = document.createElement("link");
-  cssTag.rel = "stylesheet";
-  cssTag.type = "text/css";
-  cssTag.href = "/cadastro.css";
+  let dataArquivo = false;
 
   btnCadastro.addEventListener("click", () => {
-    fetch("/modal-cadastro")
-      .then((response) => response.text())
-      .then((data) => {
-        div.innerHTML = data;
-        content.innerHTML = "";
-        content.appendChild(div);
+    if (!dataArquivo) {
+      fetch("/modal-cadastro")
+        .then((response) => response.text())
+        .then((data) => {
+          dataArquivo = data;
+          criaModal();
 
-        document.head.appendChild(cssTag);
-      })
-      .catch((err) => {
-        console.log(`Erro ao carregar o arquivo de cadastro: ${err}`);
-      });
+          criaCssTag();
+          criaJsTag();
+          btnCadastro.disabled = true;
+          document.body.style.overflow = "hidden";
+        })
+        .catch((err) => {
+          console.log(`Erro ao carregar o arquivo de cadastro: ${err}`);
+        });
+    }
+
+    if (dataArquivo) {
+      criaModal();
+
+      criaCssTag();
+      criaJsTag();
+    }
   });
+
+  function criaModal() {
+    div.innerHTML = dataArquivo;
+    content.appendChild(div);
+  }
+
+  function criaCssTag() {
+    cssTag = document.createElement("link");
+    cssTag.rel = "stylesheet";
+    cssTag.type = "text/css";
+    cssTag.href = "/cadastro.css";
+    cssTag.id = "css-cadastro";
+    document.head.appendChild(cssTag);
+  }
+
+  function criaJsTag() {
+    if (!document.getElementById("js-cadastro")) {
+      jsTag = document.createElement("script");
+      jsTag.id = "js-cadastro";
+      jsTag.src = "/cadastro.js";
+      document.head.appendChild(jsTag);
+    }
+  }
 });
