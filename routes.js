c3eab5d69d9c053cd7b56dcb655469d7bb865601
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-01-12 10:59:09 -0300

    Adiciona modal login

--- routes.js
+++ routes.js
@@ -9,10 +9,12 @@ const ControllerNotificacoes = require("./src/controllers/ControllerNotificacoes
 const ControllerPerfil = require("./src/controllers/ControllerPerfil");
 const ControllerModalCadastro = require("./src/controllers/ControllerModalCadastro");
 const ControllerCadastro = require("./src/controllers/ControllerCadastro");
+const ControllerLogin = require("./src/controllers/ControllerLogin");
 
 //Routes index
 routes.get("/", ControllerIndex.paginaIndex);
 routes.get("/modal-cadastro", ControllerModalCadastro.modalCadastro);
+routes.get("/modal-login", ControllerLogin.modalLogin);
 routes.post("/register", ControllerCadastro.register);
 
 //Routes home
