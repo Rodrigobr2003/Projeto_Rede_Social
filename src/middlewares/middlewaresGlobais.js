6706f2351b053aac9670ab4303d7c8b99975bcc9
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-01-12 15:51:03 -0300

    Ajuste login

--- src/middlewares/middlewaresGlobais.js
+++ src/middlewares/middlewaresGlobais.js
@@ -1,5 +1,6 @@
 exports.flashMessagesMiddleware = (req, res, next) => {
   res.locals.errors = req.flash("errors");
   res.locals.success = req.flash("success");
+  res.locals.user = req.session.user;
   next();
 };
