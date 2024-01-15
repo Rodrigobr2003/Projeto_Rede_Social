a3ac8767d6775b9aa196becc9071e6fd1764e70f
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-01-15 10:18:30 -0300

    Adiciona rota perfil id

--- src/controllers/ControllerPerfil.js
+++ src/controllers/ControllerPerfil.js
@@ -1,7 +1,22 @@
-exports.paginaPerfil = (req, res) => {
-  res.render("perfil", {
-    pagina: "Perfil",
-    css: "perfil",
-    script: "",
-  });
+const CadastroModel = require("../models/CadastroModel");
+
+exports.paginaPerfil = async (req, res) => {
+  try {
+    if (!req.session.user._id)
+      return res.send("Erro ao carregar pagina perfil");
+
+    const user = await CadastroModel.buscaId(req.session.user._id);
+
+    if (!user) res.render("Erro ao buscar id");
+
+    res.render("perfil", {
+      pagina: "perfil",
+      css: "perfil",
+      script: "",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send("Erro catch");
+  }
 };
