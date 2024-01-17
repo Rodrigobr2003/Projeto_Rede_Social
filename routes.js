4b481d18237f1c9dda1cfa5bb1a5789fa52cfd90
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-01-17 09:31:31 -0300

    Adiciona lógica barra de pesquisa

--- routes.js
+++ routes.js
@@ -11,6 +11,7 @@ const ControllerModalCadastro = require("./src/controllers/ControllerModalCadast
 const ControllerCadastro = require("./src/controllers/ControllerCadastro");
 const ControllerModalLogin = require("./src/controllers/ControllerModalLogin");
 const ControllerLogin = require("./src/controllers/ControllerLogin");
+const ControllerPesquisa = require("./src/controllers/ControllerPesquisa");
 
 //Middlewares
 const { loginRequired } = require("./src/middlewares/middlewaresGlobais");
@@ -24,6 +25,8 @@ routes.post("/register", ControllerCadastro.register);
 
 //Routes home
 routes.get("/home", loginRequired, ControllerHome.paginaHome);
+
+routes.get("/pesquisa-user", ControllerPesquisa.pesquisaUser);
 
 //Routes amigos
 routes.get("/amigos", loginRequired, ControllerAmigos.paginaAmigos);
