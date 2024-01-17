580c719e44d3c1ef12d74674cc78548a1fda855b
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-01-17 12:34:27 -0300

    Adiciona troca de paginas users

--- src/controllers/ControllerPerfil.js
+++ src/controllers/ControllerPerfil.js
@@ -1,16 +1,16 @@
 const CadastroModel = require("../models/CadastroModel");
 
 exports.paginaPerfil = async (req, res) => {
+  req.params._id = req.params.id.replace(":", "");
   try {
-    if (!req.session.user._id)
-      return res.send("Erro ao carregar pagina perfil");
+    if (!req.params._id) return res.send("Erro ao carregar pagina perfil");
 
-    const user = await CadastroModel.buscaId(req.session.user._id);
+    const user = await CadastroModel.buscaId(req.params._id);
 
     if (!user) res.render("Erro ao buscar id");
 
     res.render("perfil", {
-      pagina: "Perfil",
+      pagina: `Perfil - ${user.nome} ${user.sobrenome}`,
       css: "perfil",
       script: "",
       user,
