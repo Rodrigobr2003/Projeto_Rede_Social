80252f007ec1a3bb0d56c1df41573b7deae9509a
rodrigobr2003 <rodrigobr2003@hotmail.com>
2024-01-19 09:54:10 -0300

    Adiciona script descricao

--- public/assets/js/perfil.js
+++ public/assets/js/perfil.js
@@ -1,11 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   const descricao = document.querySelector(".descricao-text");
-  const btnEdit = document.querySelector(".editar-desc");
+  const divBtn = document.querySelector(".editar-desc");
+  const btnEdit = document.querySelector(".edit");
+  const btnBack = document.querySelector(".back");
+  const btnSalvar = document.querySelector(".save-desc");
+  const input = document.querySelector(".desc-input");
 
-  if (descricao.innerHTML === "") {
-    const p = document.createElement("p");
-    p.innerHTML = "Adicionar descrição";
+  const p = document.createElement("p");
+  p.innerHTML = "Adicionar descrição";
 
-    btnEdit.appendChild(p);
-  }
+  if (descricao.innerHTML === "") divBtn.appendChild(p);
+
+  btnEdit.addEventListener("click", () => {
+    input.style.display = "block";
+    btnSalvar.style.display = "block";
+    descricao.style.display = "none";
+    btnEdit.style.display = "none";
+    p.style.display = "none";
+    btnBack.style.display = "block";
+
+    input.value = `${descricao.innerHTML}`;
+  });
+
+  btnSalvar.addEventListener("click", () => {
+    input.style.display = "none";
+    btnSalvar.style.display = "none";
+    descricao.style.display = "block";
+    btnEdit.style.display = "block";
+  });
+
+  btnBack.addEventListener("click", () => {
+    input.style.display = "none";
+    btnSalvar.style.display = "none";
+    descricao.style.display = "block";
+    btnEdit.style.display = "block";
+    btnBack.style.display = "none";
+
+    if (descricao.innerHTML === "") p.style.display = "block";
+  });
 });
