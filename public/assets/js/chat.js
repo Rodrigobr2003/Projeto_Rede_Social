3a8317e86a4eab437d0b5ac783b6be162c894223
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-01-29 16:57:04 -0300

    Adiciona chat entre users

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   let rotaVolta = document.querySelector(".back");
+  let chatForm = document.querySelector("#chat-form");
 
   fetch("/mostra-amigos")
     .then((response) => response.json())
@@ -11,6 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
       nomeUser.innerHTML = `${data[0].nome} ${data[0].sobrenome}`;
 
       rotaVolta.href = `/searched-user/${idUser}`;
+
+      //Chat
+      const socket = io();
+
+      //Entrar no chat
+      socket.emit("joinChat", data[0].nome);
+
+      socket.on("alert", (msg) => {
+        alerta(msg);
+      });
+
+      function alerta(msg) {
+        const divFriend = document.querySelector(".friend-msg").cloneNode(true);
+        const divFriendSide = document.querySelector(".friend-side");
+
+        divFriend.innerHTML = msg;
+        divFriend.style.display = "flex";
+        divFriend.classList.add("notf");
+
+        divFriendSide.append(divFriend);
+      }
+
+      chatForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let msg = e.target.msg.value;
+
+        e.target.msg.value = "";
+
+        socket.emit("chatMessage", msg);
+      });
+
+      socket.on("message", (msg) => {
+        mensagem(msg);
+      });
+
+      function mensagem(msg) {
+        const divUser = document.querySelector(".user-msg").cloneNode(true);
+        const divUserSide = document.querySelector(".user-side");
+
+        divUser.innerHTML = msg;
+        divUser.style.display = "flex";
+
+        divUserSide.appendChild(divUser);
+      }
     })
     .catch((err) => console.log(err));
 });
