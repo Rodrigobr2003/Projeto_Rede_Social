75a45063282df9a93d61fb07756a11421cf39a4b
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-02-09 15:50:11 -0300

    Adiciona arquivos mensagem

--- src/controllers/ControllerSearchedProfile.js
+++ src/controllers/ControllerSearchedProfile.js
@@ -127,37 +127,6 @@ exports.amigosSearchedProfile = async (req, res) => {
   res.json(pesquisaUserBd);
 };
 
-exports.chatProfile = (req, res) => {
-  res.render("chatProfile", {
-    pagina: "Chat",
-    css: "chat",
-    script: "chat",
-  });
-};
-
-exports.salvaMensagens = async (req, res) => {
-  const idUser = req.session.user._id;
-
-  const mensagem = new Mensagem(req.body.message.texto);
-  await mensagem.registrarMensagem(req.body.chatRoom, idUser);
-
-  res.json(mensagem);
-};
-
-exports.carregaMensagens = async (req, res) => {
-  const room = req.body.chatRoom;
-
-  const data = new Mensagem();
-  const modelMensagem = await data.carregaMensagens(room);
-  const mensagens = modelMensagem.mensagem;
-
-  if (mensagens == "") {
-    return res.json(mensagens);
-  }
-
-  res.json(mensagens.mensagem);
-};
-
 exports.procurarPerfilId = async (req, res) => {
   const id = req.body.userId;
 
